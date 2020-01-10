a8129c12029bc45377ff3abac8f2abed259468cc
xpbsm <dewxpbsm@gmail.com>
2020-01-10 16:21:23 +0800

    Initial commit from gatsby: (https://github.com/ammarjabakji/gatsby-markdown-blog-starter.git)

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => (
+  <>
+    <h1>
+        Edit About component or pages/about.js to include your information.
+    </h1>
+  </>
+)
+
+export default About
