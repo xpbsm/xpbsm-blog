f99f0d582bb744619de495bb5f9b45bd8dd437a0
xpbsm <dewxpbsm@gmail.com>
2020-01-15 17:00:58 +0800

    add cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-htaccess",
     {
       resolve: "gatsby-source-filesystem",
